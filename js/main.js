a935fc7960f0911cd9394049c81974ecb4420506
saiazar <diseno.saideazar@gmail.com>
2016-11-16 01:32:20 -0600

    juego de la computadora

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ console.log(jugador);
 
 //Número random 1-3
 var computadora = Math.ceil((Math.random()*3));
+
+//Juego de la computadora
+console.log(computadora);
+	if (computadora == 1){
+		computadora = "piedra";
+	}else if (computadora == 2){
+		computadora = "papel";
+	}else if (computadora == 3){
+		computadora = "tijeras";
+	}
+console.log(computadora);
+comparar(jugador, computadora);
+alert("Computadora escogió: "+ computadora + ".\n" +
+	"El jugador escogió: " + jugador + ".\n" +
+	"Por lo tanto: " + resultado);
+console.log("Computadora escogió: "+ computadora + ".\n" +
+	"El jugador escogió: " + jugador + ".\n" +
+	"Por lo tanto: " + resultado);

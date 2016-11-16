2bb91aa8879c5001a7985b31af6a3ee8bae19ef3
saiazar <diseno.saideazar@gmail.com>
2016-11-16 01:30:46 -0600

    selección de número random

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //Decisión del jugador
 var respuesta;
 var jugador = prompt ("¿Piedra, papel o tijeras?");
+jugador = jugador.toLowerCase();
 console.log(jugador);
 
+//Número random 1-3
+var computadora = Math.ceil((Math.random()*3));
